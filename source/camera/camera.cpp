7534b20efaa20f054c9e1531602f91b689d8a8ac
knz13 <82895172+knz13@users.noreply.github.com>
2022-02-23 02:38:24 -0300

    Update camera.cpp

--- source/camera/camera.cpp
+++ source/camera/camera.cpp
@@ -39,10 +39,7 @@ const glm::vec3& Camera::GetPosition() {
 }
 
 glm::mat4 Camera::GetViewProjection(const Window& window) {
-    return glm::perspective(m_Fov,(float)window.Properties().width/window.Properties().height,m_DrawNear,m_DrawDistance) * glm::inverse(glm::translate(glm::mat4(1.0f),m_Position) 
-    *glm::rotate(glm::mat4(1.0f),m_Rotation.x,glm::vec3(1,0,0))
-    *glm::rotate(glm::mat4(1.0f),m_Rotation.y,glm::vec3(0,1,0))
-    *glm::rotate(glm::mat4(1.0f),m_Rotation.z,glm::vec3(0,0,1)));
+    return glm::perspective(m_Fov,(float)window.Properties().width/window.Properties().height,m_DrawNear,m_DrawDistance) * glm::inverse(glm::translate(glm::mat4(1.0f),m_Position)* glm::toMat4(glm::quat(m_Rotation)));
 }
 
 Camera::Camera() {
